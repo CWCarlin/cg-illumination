d8452ccbd68a0c9cc483e144a3c3b1c6f298c0df
elott01 <ethanl1212@gmail.com>
2024-05-13 21:54:09 -0500

    created scene 3

--- src/renderer.js
+++ src/renderer.js
@@ -48,6 +48,17 @@ class Renderer {
                 ambient: new Color3(0.2, 0.2, 0.2),
                 lights: [],
                 models: []
+            },
+            {
+                scene: new Scene(this.engine),
+                background_color: new Color4(0.2, 0.01, 0.2, 1.0),
+                materials: null,
+                ground_subdivisions: [500, 500],
+                ground_mesh: null,
+                camera: null,
+                ambient: new Color3(0.2, 0.2, 0.2),
+                lights: [],
+                models: []
             }
         ];
         this.active_scene = 0;
@@ -451,6 +462,113 @@ class Renderer {
             }
             sphere.material = materials['illum_' + this.shading_alg];
             current_scene.models.push(sphere);
+        }
+
+
+        scene.onKeyboardObservable.add((key_press) => {
+            switch (key_press.event.key) {
+                case 'w':
+                    current_scene.lights[this.active_light].position.addInPlace(new Vector3(0.0, 0.0, -0.5));
+                    break;
+                case 'a':
+                    current_scene.lights[this.active_light].position.addInPlace(new Vector3(-0.5, 0.0, 0.0));
+                    break;
+                case 's':
+                    current_scene.lights[this.active_light].position.addInPlace(new Vector3(0.0, 0.0, 0.5));
+                    break;
+                case 'd':
+                    current_scene.lights[this.active_light].position.addInPlace(new Vector3(0.5, 0.0, 0.0));
+                    break;
+                case 'r':
+                    current_scene.lights[this.active_light].position.addInPlace(new Vector3(0.0, 0.5, 0.0));
+                    break;
+                case 'f':
+                    current_scene.lights[this.active_light].position.addInPlace(new Vector3(0.0, -0.5, 0.0));
+                    break;
+            }
+        });
+
+        scene.onBeforeRenderObservable.add(() => {
+            // update models and lights here (if needed)
+            // ...
+
+            // update uniforms in shader programs
+            this.updateShaderUniforms(scene_idx, materials['illum_' + this.shading_alg]);
+            this.updateShaderUniforms(scene_idx, materials['ground_' + this.shading_alg]);
+        });
+    }
+
+    createScene3(scene_idx) {
+        let current_scene = this.scenes[scene_idx];
+        let scene = current_scene.scene;
+        let materials = current_scene.materials;
+        let ground_mesh = current_scene.ground_mesh;
+
+        // Set scene-wide / environment values
+        scene.clearColor = current_scene.background_color;
+        scene.ambientColor = current_scene.ambient;
+        scene.useRightHandedSystem = true;
+
+        // Create camera
+        current_scene.camera = new UniversalCamera('camera', new Vector3(0.0, 1.8, 10.0), scene);
+        current_scene.camera.setTarget(new Vector3(0.0, 1.8, 0.0));
+        current_scene.camera.upVector = new Vector3(0.0, 1.0, 0.0);
+        current_scene.camera.attachControl(this.canvas, true);
+        current_scene.camera.fov = 35.0 * (Math.PI / 180);
+        current_scene.camera.minZ = 0.1;
+        current_scene.camera.maxZ = 100.0;
+
+        // Create point light sources
+        let light0 = new PointLight('light0', new Vector3(1.0, 1.0, 5.0), scene);
+        light0.diffuse = new Color3(1.0, .99, .91);
+        light0.specular = new Color3(1.0, 1.0, 1.0);
+        current_scene.lights.push(light0);
+
+        // Create point light sources
+        let light1 = new PointLight('light1', new Vector3(1.0, 1.0, 5.0), scene);
+        light1.diffuse = new Color3(0, 1.0, 0);
+        light1.specular = new Color3(1.0, 1.0, 1.0);
+        current_scene.lights.push(light1);
+
+                        // Create point light sources
+        let light2 = new PointLight('light2', new Vector3(2.0, 1.0, 5.0), scene);
+        light2.diffuse = new Color3(0, 0.6, 0.5);
+        light2.specular = new Color3(1.0, 1.0, 1.0);
+        current_scene.lights.push(light2);
+
+        // Create ground mesh
+        let white_texture = RawTexture.CreateRGBTexture(new Uint8Array([255, 255, 255]), 1, 1, scene);
+        let ground_heightmap = new Texture(BASE_URL + 'heightmaps/fuji.png', scene);
+        ground_mesh.scaling = new Vector3(20.0, 1.0, 20.0);
+        ground_mesh.metadata = {
+            mat_color: new Color3(0.10, 0.85, 0.55),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.0, 0.0, 0.0),
+            mat_shininess: 1,
+            texture_scale: new Vector2(1.0, 1.0),
+            height_scalar: 1.0,
+            heightmap: ground_heightmap
+        }
+        ground_mesh.material = materials['ground_' + this.shading_alg];
+        
+
+        for (let i = 0; i <= 30; i ++) {
+            // Create other models
+            let box = CreateBox('box' + i, {size: 1}, scene);
+            let angle = (i / 20) * Math.PI * 2; // Calculate angle for circular orientation
+            let radius = 5.0;
+            let x = radius * Math.cos(angle);
+            let z = radius * Math.sin(angle);
+            box.position = new Vector3(x, 3.0, z);
+            box.metadata = {
+                mat_color: new Color3(i/10, i/10, i/10),
+                mat_texture: white_texture,
+                mat_specular: new Color3(1.0, 1.0, 1.0),
+                mat_shininess: 10,
+                texture_scale: new Vector2(1.0, 1.0)
+            }
+            box.material = materials['illum_' + this.shading_alg];
+            current_scene.models.push(box);
         }
 
 
